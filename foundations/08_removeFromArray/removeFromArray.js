c3b5b96c2758151d17bb8b77d6dc7c447f1914e9
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-16 17:45:38 -0600

    Add removeArray function
    Function has not been tested and can be simplified, currently using
    nested loops

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+
+const removeFromArray = function (arr, ...removal) {
+    let returnArray = arr.slice();
+    nextItem:
+    for (let removalItem of removal) {
+        for (let checkItem of returnArray) {
+            if (checkItem == removalItem) {
+                returnArray.splice(returnArray.indexOf(checkItem), 1);
+                continue nextItem;
+            } continue;
+        }
+    } return returnArray;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 2, 3));
+
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
