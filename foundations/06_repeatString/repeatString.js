7012eec05f5d2257bd7eea36cffe62b60669c547
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-16 15:56:16 -0600

    Add repeatString function
    repeatString function behaves as expected

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num) {
+    let result = "";
+    if (num < 0) return "ERROR";
+    if (num > 0) {
+        for (let stringCount = 0; stringCount < num; stringCount++) {
+            result += string;
+        }
+    } return result;
+}
+const result = repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
