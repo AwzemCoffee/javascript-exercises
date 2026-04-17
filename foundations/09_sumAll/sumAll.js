68a2a7b97c90668d7a30c236f1ec9316070cb442
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-16 19:15:44 -0600

    Format adjustments
    Added formatting for better readability of function block

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,7 +2,9 @@ const sumAll = function (min, max) {
     if (min < 0 ||
         !Number.isInteger(min) ||
         !Number.isInteger(max)) return "ERROR";
+
     if (min > max) [min, max] = [max, min];
+
     let range = [];
     for (let i = min; i <= max; i++) {
         range.push(i);
