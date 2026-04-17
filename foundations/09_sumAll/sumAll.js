51fd65caefa553018d2d15506f756c0b226a0421
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-16 19:09:12 -0600

    Add sumAll function
    sumAll function is behaving as expected for all use cases.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+    if (min < 0 ||
+        !Number.isInteger(min) ||
+        !Number.isInteger(max)) return "ERROR";
+    if (min > max) [min, max] = [max, min];
+    let range = [];
+    for (let i = min; i <= max; i++) {
+        range.push(i);
+    }
+    let result = range.reduce((sum, digit) => sum + digit, 0)
+    return result;
 };
 
 // Do not edit below this line
