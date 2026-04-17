0a3c5465c2eaa5a5f97f9958efbeedb272c67b04
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-16 19:56:02 -0600

    Improve readability of leapYears
    leapYears was too abstract, now uses variable names for conditionals.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,12 +1,24 @@
-const leapYears = function (year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            } return false;
-        } return true;
-    } return false;
-};
+// const leapYears = function (year) {
+//     if (year % 4 == 0) {
+//         if (year % 100 == 0) {
+//             if (year % 400 == 0) {
+//                 return true;
+//             } return false;
+//         } return true;
+//     } return false;
+// };
 
+const leapYears = function (year) {
+    const divisibleByFour = (year % 4 === 0);
+    const divByHundred = (year % 100 === 0);
+    const divByFourHundred = (year % 400 === 0);
+
+    if (
+        divisibleByFour &&
+        (!divByHundred) || (divByFourHundred)
+    ) {
+        return true;
+    } else return false;
+}
 // Do not edit below this line
 module.exports = leapYears;
