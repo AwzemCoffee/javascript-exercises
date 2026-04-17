47815737fe5a32f8c2a892716352e2b7d79caff0
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-16 18:04:28 -0600

    optimize the removeFromArray function
    removeFromArray was using nested loops which was quite confusing and
    complicated

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,18 +1,9 @@
 
 const removeFromArray = function (arr, ...removal) {
-    let returnArray = arr.slice();
-    nextItem:
-    for (let removalItem of removal) {
-        for (let checkItem of returnArray) {
-            if (checkItem == removalItem) {
-                returnArray.splice(returnArray.indexOf(checkItem), 1);
-                continue nextItem;
-            } continue;
-        }
-    } return returnArray;
+    return arr.filter(item => !removal.includes(item));
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 2, 3));
+//console.log(removeFromArray([1, 2, 3, 4], 2));
 
 // Do not edit below this line
-//module.exports = removeFromArray;
+module.exports = removeFromArray;
