7b12e7c1d7e24b070cdb18d768770a6f2e22d7fb
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-17 18:09:53 -0600

    Add temperature convert functions
    Functions pass all tests, work as expected.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+    let toCelsius = ((tempF - 32) * 5 / 9);
+    toCelsius = Math.round(toCelsius * 10) / 10;
+    return toCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+    let toFahrenheit = (tempC * 9 / 5 + 32);
+    toFahrenheit = Math.round(toFahrenheit * 10) / 10;
+    return toFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
