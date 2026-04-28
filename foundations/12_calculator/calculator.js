b848116829d9fee7f914cd8556ae03684c7d0645
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-28 13:41:29 -0600

    Complete upto fibonnaci

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (a, b) {
+    return (a + b);
 
 };
 
-const power = function() {
-	
+const subtract = function (a, b) {
+    return (a - b);
 };
 
-const factorial = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((sum, digit) => {
+        return (sum + digit);
+    }, 0);
+};
+
+const multiply = function (arr) {
+    return arr.reduce((product, digit) => {
+        return (product * digit);
+    });
+};
+
+const power = function (base, exponent) {
+    return (base ** exponent);
+};
+
+const factorial = function (digit) {
+    if (digit === 0) return 1;
+    let factors = [];
+    for (let factor = 1; factor <= digit; factor++) {
+        factors.push(factor);
+    }
+    return factors.reduce((factorial, nextNum) => {
+        return (factorial * nextNum);
+    });
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (inputStr) {
+    let lowerInputStr = inputStr.toLowerCase();
+    let cleanArray = Array.from(lowerInputStr)
+        .filter(character => /[a-zA-Z0-9]/.test(character));
+    let reversedCleanArray = cleanArray.toReversed();
+    return reversedCleanArray.toString() === cleanArray.toString();
 };
 
 // Do not edit below this line
