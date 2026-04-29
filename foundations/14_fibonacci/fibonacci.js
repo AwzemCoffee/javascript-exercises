ef6ec3bb50e0e3d7be1a7ad463bb899128add637
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-04-29 06:12:47 -0600

    Complete fibonacchi sequence
    This method builds an entire array.
    
    Should try sliding window method later.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    if (index < 0) return "OOPS";
+    let fibSequenceSeed = [0, 1];
+    for (let nextIndex = 2; nextIndex <= index; nextIndex++) {
+        let currentMax = (fibSequenceSeed.length - 1);
+        let nextNumber = fibSequenceSeed[currentMax] + fibSequenceSeed[currentMax - 1];
+        fibSequenceSeed.push(nextNumber);
+    }
+    return fibSequenceSeed[index];
 };
 
 // Do not edit below this line
